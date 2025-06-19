01da7bca9401e4cdac22f1d04da5ae13a475c0d4
Szymon Wyrzykowski <simon@theglitch.co>
2025-06-19 17:02:26 +0200

    path correction for new domain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   // use the *exact* repo name between the slashes
-  base: '/gloupsoup/',        // or '/gloup-soup/' if that is the repo
+  base: './',        // or '/gloup-soup/' if that is the repo
   plugins: [react()],
 });
